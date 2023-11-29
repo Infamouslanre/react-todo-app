761dddb93b35859407161256ce3ff687735a921c
Infamouslanre <mdaramola92@gmail.com>
2023-11-29 17:20:39 +0000

    testing redeploy

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>React Todo App</h1>
+      <h1>Azure Todo App</h1>
       <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
       <TodoList list={todos} remove={deleteTodo} />
     </div>
